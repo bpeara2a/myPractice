eacfd90dbe7f9a6600b06efd5fb245a7d9d291a8
bpeara2a <bpeara2a@gmail.com>
2020-12-27 00:29:00 -0500

    Added Practice to Github

---
+++ Pactice/src/arrays/ArrayDemo.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		
+		int[] a = {10,20,30,40};
+		
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
